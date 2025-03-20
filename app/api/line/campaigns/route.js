f4832e509af9c3d40fb5aca18fc9e9cbae3d8d73
Dangpeerawit24 <dang.peerawit24@gmail.com>
2025-03-20 07:59:06 +0700

    d

--- app/api/line/campaigns/route.js
+++ app/api/line/campaigns/route.js
@@ -5,12 +5,18 @@ const prisma = new PrismaClient();
 
 export async function GET() {
     try {
-        const campaigns = await prisma.campaign.findMany({
-            where: { status: "เปิดกองบุญ" },
-            orderBy: { id: 'desc' }
-        });
+        const campaigns = await prisma.$queryRaw`
+            SELECT c.*, 
+                COALESCE(SUM(t.value), 0) AS total_value
+            FROM Campaign c
+            LEFT JOIN Campaign_transactions t ON c.id = t.campaignsid
+            WHERE c.status = 'เปิดกองบุญ'
+            GROUP BY c.id
+            HAVING c.stock > COALESCE(SUM(t.value), 0)
+            ORDER BY c.updated_at DESC;
+        `;
 
-        return NextResponse.json(campaigns); 
+        return NextResponse.json(campaigns);
     } catch (error) {
         console.error("Database Error:", error);
         return NextResponse.json({ error: "Failed to fetch campaigns" }, { status: 500 });
